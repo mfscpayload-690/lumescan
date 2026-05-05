cd456b2819057e91356663d8223aa4d1efa10bb4
Aravind Lal <aravindlal2006@gmail.com>
2026-05-05 08:43:47 +0530

    chore(web): revert static export to support dynamic web service hosting

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  /* Standard config for Web Service deployment */
   images: {
     unoptimized: true,
   },
